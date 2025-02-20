537951c860fdbd932fffacc4256f332e6eb631dd
Darshan Bagade <darshanbagade123@gmail.com>
2025-02-20 06:00:00 +0530

    fix deployment errors

--- Sentiment-Analysis-Project/test_scraper.py
+++
@@ -1,8 +0,0 @@
-from scraper import fetch_reviews_api
-
-product_url = "https://www.amazon.in/dp/B0CHX1W1XY"  
-reviews = fetch_reviews_api(product_url)
-
-print("Fetched Reviews:")
-for review in reviews:
-    print("-", review)
